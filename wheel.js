86828bf59092a91e7792ec607e97cc7bde7a82e5
oyo <303451715@qq.com>
2016-10-10 14:18:23 +0800

    Add files via upload

---
+++ wheel.js
@@ -0,0 +1,132 @@
+/**
+ * Created by Administrator on 2016/10/9.
+ */
+_ = require('lodash')
+
+function syncAndAsync(Tasks, callback){
+    if(typeof Tasks != 'array'){
+        err = "no functionArray";
+        return callback(err, null);
+    }
+    var syncResult = {};
+    var allResult = [];
+    var asyncIndex = 0;
+    var syncIndex = 0;
+    var asyncLength = 0;
+    var syncLength = 0;
+    
+    function eachOf(coll, iteratee, callback){
+        var index = 0,
+            length = coll.length;
+        if(length == 0){
+            callback(null);
+        }
+        
+        function iterateeCallback(err){
+            if(err){
+                callback(err);
+            }else if(++syncIndex == length && asyncLength==asyncIndex){
+                callback(null);
+            }
+        }
+        
+        for(; index < length; index++){
+            iteratee(coll[index], index, onlyOnce(iterateeCallback));
+        }
+    }
+    
+    function nextTask(asyncTask, args){
+        if(asyncIndex == asyncLength && syncIndex == syncLength){
+            allResult['async'] = [null].concat(args);
+            return callback(null, allResult);
+        }
+        var taskCallback = onlyOnce(baseRest(function(err, args){
+            if(err){
+                allResult['async'] = [err].concat(args);
+                return callback(null, allResult);
+            }
+            nextTask(asyncTask, args);
+        }));
+        
+        args.push(taskCallback);
+        var task = asyncTask[asyncIndex++];
+        task.apply(null, args);
+    }
+    
+    for(var i = 0; i< Tasks.length; index++){
+        if(i === 0){
+            syncLength = Tasks[0].length;
+        }else{
+            asyncLength = Tasks[1].length
+        }
+    }
+    
+    for(var index = 0; index< Tasks.length; index++){
+        if(index == 0){
+            eachOf(Tasks[0], function(task, index, callback1){
+                task(baseRest(function(err, args){
+                    if(args.length <= 1){
+                        args = args[0];
+                    }
+                    syncResult[index] = args;
+                    callback1(err);
+                }));
+            }, function(err){
+                callback(err, allResult);
+            });
+        }
+        else{
+            nextTask(Tasks[1], []);
+        }
+    }
+    
+    setTimeout(function(){
+        if(_.findIndex(allResult, 'async') != -1 && _.findIndex(allResult, 'sync') != -1 ){
+            callback(null, allResult);
+        }
+    }, 1000);
+    
+    
+    
+}
+
+function onlyOnce(fn){
+    return function(){
+        if(fn === null)
+            return
+        var callFn = fn;
+        fn = null;
+        callFn.apply(this, arguments):
+    }
+}
+
+
+function baseRest(func){
+    start = func.length -1;
+    return function(){
+        var args = arguments,
+            index = -1,
+            length = args.length - start,
+            array = Array(length);
+        while(++index < length){
+            array[index] = args[index+start]
+        }
+        index = -1;
+        var otherArgs = Array[start+1];
+        while(++index< start){
+            otherArgs[index] = args[index]
+        }
+        otherArgs[start] = array;
+        return apply(func, this, otherArgs);
+    }
+}
+
+function apply(func, thisArg, args){
+    switch(args.length){
+        case 0: return func.call(thisArg);
+        case 1: return func.call(thisArg, args[0]);
+        case 2: return func.call(thisArg, args[0], args[1]);
+        case 3: return func.call(thisArg, args[0], args[1], args[2]);
+    }
+    return func.apply(thisArg, args);
+}
